034be4426a5a199bc389846f0391f4151e84dd15
Davit Petrosyan <davit.petrosyan.sc@gmail.com>
2020-09-10 23:32:20 +0400

    Multifile example

---
+++ lesson-1/multifile/main.cpp
@@ -0,0 +1,7 @@
+void printMyMessage(const char* message);
+
+int main(){
+    printMyMessage("Hello");
+
+    return 0;
+}
---
+++ lesson-1/multifile/message.cpp
@@ -0,0 +1,5 @@
+#include <cstdio>
+
+void printMyMessage(const char* message){
+	printf("Message: %s\n", message);
+}
