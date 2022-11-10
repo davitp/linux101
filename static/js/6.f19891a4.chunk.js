005787fed6e8bc7d289c433af5eb568e8c7fab37
Davit Petrosyan <7922751+davitp@users.noreply.github.com>
2022-11-10 17:45:18 +0400

    Updates

--- static/js/6.f19891a4.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonplinux101=this.webpackJsonplinux101||[]).push([[6],{209:function(t,e,r){"use strict";r.r(e);var n=r(211),c=r(195),i=(r(0),r(7)),s=r(57),a=r(8);e.default=function(){var t=Object(i.m)();return Object(a.jsxs)(a.Fragment,{children:[Object(a.jsx)(s.a,{children:Object(a.jsx)("title",{children:"Error 404"})}),Object(a.jsx)(n.a,{status:"404",title:"Error 404",subTitle:"The requested page is not found",extra:Object(a.jsx)(c.a,{type:"primary",onClick:function(){return t(-1)},children:"Go Back"})})]})}}}]);
-//# sourceMappingURL=6.f19891a4.chunk.js.map
---
+++ static/js/7.d6c735e9.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonplinux101=this.webpackJsonplinux101||[]).push([[7],{212:function(t,e,r){"use strict";r.r(e);var n=r(215),c=r(195),i=(r(0),r(7)),s=r(56),a=r(8);e.default=function(){var t=Object(i.m)();return Object(a.jsxs)(a.Fragment,{children:[Object(a.jsx)(s.a,{children:Object(a.jsx)("title",{children:"Error 404"})}),Object(a.jsx)(n.a,{status:"404",title:"Error 404",subTitle:"The requested page is not found",extra:Object(a.jsx)(c.a,{type:"primary",onClick:function(){return t(-1)},children:"Go Back"})})]})}}}]);
+//# sourceMappingURL=7.d6c735e9.chunk.js.map
